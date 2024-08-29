9f6fe930fd1d795c8ff445d18df3bcfaca31fc58
LEEEUNCHEOL <dmscjf1020@naver.com>
2024-08-29 18:04:41 +0900

    6단계 삭제추가완료

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 public class App {
     Scanner sc;
 
-    App (Scanner sc) {
+    App(Scanner sc) {
         this.sc = sc;
     }
 
-    public void run () {
+    public void run() {
         int lastId = 1;
         List<Article> articleList = new ArrayList<>();
 
         System.out.println("== 게시판 앱 ==");
+
         while (true) {
             System.out.print("명령) ");
             String command = sc.nextLine().trim();
@@ -35,11 +36,25 @@ public class App {
                 System.out.println("번호 / 제목 / 내용");
                 System.out.println("-----------------");
 
-                for (int i = 0; i < articleList.size(); i++) {
+                for (int i = articleList.size() - 1; i >= 0; i--) { //역순으로 출력
                     Article article = articleList.get(i);
                     System.out.printf("%d / %s / %s\n", article.getId(), article.getSubject(), article.getContent());
                 }
 
+            } else if (command.startsWith("삭제")) { //삭제 기능 추가
+                String[] comandList = command.split("\\?", 2); // ? 정규화 할때는 역슬레쉬를 사용!!!
+                String[] comandList2 = comandList[1].split("=", 2);
+
+                String key = comandList2[0];
+                String removeId = comandList2[1];
+                int reId = Integer.parseInt(removeId);
+
+                for (int i = 0; i < articleList.size(); i ++) {
+                    if (articleList.get(i).getId() == reId){
+                        articleList.remove(i);
+                    }
+                }
+                System.out.printf("%d번 게시물이 삭제되었습니다.\n", reId);
             }
         }
     }
