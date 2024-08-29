2a8f23ec495da22aa2f968dec507f25a10dc5633
LEEEUNCHEOL <dmscjf1020@naver.com>
2024-08-29 18:09:53 +0900

    7단계 삭제예외처리 추가

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -36,25 +36,31 @@ public class App {
                 System.out.println("번호 / 제목 / 내용");
                 System.out.println("-----------------");
 
-                for (int i = articleList.size() - 1; i >= 0; i--) { //역순으로 출력
+                for (int i = articleList.size() - 1; i >= 0; i--) {
                     Article article = articleList.get(i);
                     System.out.printf("%d / %s / %s\n", article.getId(), article.getSubject(), article.getContent());
                 }
 
-            } else if (command.startsWith("삭제")) { //삭제 기능 추가
-                String[] comandList = command.split("\\?", 2); // ? 정규화 할때는 역슬레쉬를 사용!!!
+            } else if (command.startsWith("삭제")) {
+                String[] comandList = command.split("\\?", 2);
                 String[] comandList2 = comandList[1].split("=", 2);
 
                 String key = comandList2[0];
                 String removeId = comandList2[1];
                 int reId = Integer.parseInt(removeId);
 
-                for (int i = 0; i < articleList.size(); i ++) {
-                    if (articleList.get(i).getId() == reId){
-                        articleList.remove(i);
+                Article article = null;
+                for (int i = 0; i < articleList.size(); i++) {
+                    if (articleList.get(i).getId() == reId) {
+                        article = articleList.get(i);
                     }
                 }
-                System.out.printf("%d번 게시물이 삭제되었습니다.\n", reId);
+                if (article == null) {  // 삭제 예외처리 추가
+                    System.out.printf("%d번 게시물은 존재하지 않습니다.\n", reId);
+                } else {
+                    articleList.remove(article);
+                    System.out.printf("%d번 게시물이 삭제되었습니다.\n", reId);
+                }
             }
         }
     }
