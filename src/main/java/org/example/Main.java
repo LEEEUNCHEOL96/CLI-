e075c32845f9b66004397477769e0992ad7edc66
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-08-29 12:41:59 +0900

    4단계완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,19 +7,22 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         System.out.println(" == 게시판 앱 == ");
+        int postId = 1;
+
 
         while(true){
             System.out.print("명령 ) ");
-            String command = sc.nextLine();
+            String command = sc.nextLine().trim();
 
             if(command.equals("종료")){
                 break;
             } else if (command.equals("등록")) {
                 System.out.print("제목 : ");
-                sc.nextLine();
+                sc.nextLine().trim();
                 System.out.print("내용 : ");
-                sc.nextLine();
-                System.out.println("1번 게시물이 등록되었습니다.");
+                sc.nextLine().trim();
+                System.out.printf("%d번 게시물이 등록되었습니다.\n",postId);
+                postId ++;
             }
         }sc.close();
     }
