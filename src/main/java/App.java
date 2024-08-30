6ed01a5369f02f4d9950a596cd53dcf1a44fbbf1
LEEEUNCHOEL <dmscjf1020@gmail.com>
2024-08-30 09:11:27 +0900

    lv_11_complete

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,8 +1,6 @@
 import article.Article;
 import article.ArticleController;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -25,77 +23,18 @@ public class App {
             if (command.equals("종료")) {
                 break;
             } else if (command.equals("등록")) {
-                System.out.print("제목 : ");
-                String subject = sc.nextLine().trim();
-                System.out.print("내용 : ");
-                String content = sc.nextLine().trim();
-                System.out.printf("%d번 게시물이 등록되었습니다.\n", lastId);
+                articleController.write();
 
-                Article article = new Article(lastId, subject, content);
-                articleList.add(article);
-
-                lastId++;
             } else if (command.equals("목록")) {
-                System.out.println("번호 / 제목 / 내용");
-                System.out.println("-----------------");
-
-                for (int i = articleList.size() - 1; i >= 0; i--) {
-                    Article article = articleList.get(i);
-                    System.out.printf("%d / %s / %s\n", article.getId(), article.getSubject(), article.getContent());
-                }
+                articleController.list();
 
             } else if (command.startsWith("삭제")) {
-                String[] comandList = command.split("\\?", 2);
-                String[] comandList2 = comandList[1].split("=", 2);
+                articleController.delete(command);
 
-                String key = comandList2[0];
-                String removeId = comandList2[1];
-                int id2 = Integer.parseInt(removeId);
-
-                Article article = _getFindId(id2);
-
-                if (article == null) {
-                    System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id2);
-                } else {
-                    articleList.remove(article);
-                    System.out.printf("%d번 게시물이 삭제되었습니다.\n", id2);
-                }
             } else if (command.startsWith("수정")) {
-                String[] comandList = command.split("\\?", 2);
-                String[] comandList2 = comandList[1].split("=", 2);
+                articleController.modify(command);
 
-                String key = comandList2[0];
-                String modifiedId = comandList2[1];
-                int id2 = Integer.parseInt(modifiedId);
-
-
-                Article article = _getFindId(id2);
-
-                if (article == null) {
-                    System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id2);
-                } else {
-                    System.out.printf("제목(기존) : %s \n", article.getSubject());
-                    System.out.print("제목 : ");
-                    String modifySubject = sc.nextLine();
-                    article.setSubject(modifySubject);
-
-                    System.out.printf("내용(기존) : %s \n", article.getContent());
-                    System.out.print("내용 : ");
-                    String modifyContent = sc.nextLine();
-                    article.setModify(modifyContent);
-
-                    System.out.printf("%d번 게시물이 수정 되었습니다. \n", id2);
-                }
             }
         }
-    }
-
-    private Article _getFindId(int id2) { //중복로직 제거
-        for (Article i : articleList) {
-            if (i.getId() == id2) {
-                return i;
-            }
-        }
-        return null;
     }
 }
--- src/main/java/article/Article.java
+++ src/main/java/article/Article.java
@@ -1,30 +1,30 @@
 package article;
 
 public class Article {
-    private int id;
+    private final int id;
     private String subject;
     private String content;
 
-    Article (int id, String subject, String content) {
+    Article(int id, String subject, String content) {
         this.id = id;
         this.subject = subject;
         this.content = content;
     }
 
-    int getId () {
+    int getId() {
         return this.id;
     }
 
-    String getSubject () {
+    String getSubject() {
         return this.subject;
-    }
-
-    String getContent() {
-        return this.content;
     }
 
     public void setSubject(String subject) { //수정 제목 저장
         this.subject = subject;
+    }
+
+    String getContent() {
+        return this.content;
     }
 
     public void setModify(String content) { // 수정 내용 저장
